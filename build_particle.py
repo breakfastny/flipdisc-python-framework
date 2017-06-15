e332627c86c84636f272a40d47ba16b464b22beb
Guilherme Polo <gp@luster.cc>
2017-06-15 09:43:54 -0300

    accept FDL_INCLUDE_DIR env for overriding the default /usr/local/include/fdl path

--- build_particle.py
+++ build_particle.py
@@ -3,14 +3,20 @@ import os
 from cffi import FFI
 
 HERE = os.path.dirname(os.path.abspath(__file__))
+FDL_PATH = os.getenv('FDL_INCLUDE_DIR', '/usr/local/include/fdl')
+PARTICLE_H = os.path.join(FDL_PATH, 'particle.h')
+if not os.path.exists(FDL_PATH):
+    raise SystemExit('** ERROR: FDL_INCLUDE_DIR "%s" does not exist' % FDL_PATH)
+if not os.path.exists(PARTICLE_H):
+    raise SystemExit('** ERROR: FDL_INCLUDE_DIR does not contain "particle.h"')
+
 
 ffibuilder = FFI()
-
 ffibuilder.set_source('flipdisc._particle',
         '#include "fdl/particle.h"',
         libraries=['fparticle'])
 
-raw_cdef = open(os.path.join(HERE, '../flipdisc-core/src/fdl/particle.h')).read()
+raw_cdef = open(PARTICLE_H).read()
 # Skip includes and other things. This assumes the first definition
 # in particle.h starts with a struct.
 raw_start = raw_cdef.find('struct ')
