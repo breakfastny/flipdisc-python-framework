e6b68de15158f1a77d9638351baca915183e6fdf
Guilherme Polo <gp@luster.cc>
2017-08-14 09:39:11 -0500

    0.4.10

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def run_setup():
 
     setup(
         name='flipdisc',
-        version='0.4.9',
+        version='0.4.10',
         url='https://github.com/breakfastny/flipdisc-python-framework',
         packages=['flipdisc', 'flipdisc.framework'],
         package_data={
